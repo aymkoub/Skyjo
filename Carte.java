fad93fc0b12fb4e1d7d8a10b2654b750994542ba
aleroux <aymeric.leroux@unistra.fr>
2025-04-25 10:39:22 +0200

    Ajout de la méthode retoruner() et attribut de visibilité

--- Carte.java
+++ Carte.java
@@ -1,15 +1,27 @@
 public class Carte {
-    private int valeur;
+    // Attributs de la classe Carte
+    private int __valeur;
+    private boolean __faceCachee;
 
-    public Carte(int val){
-        this.valeur = val;
+    // Constructeur de la classe Carte
+    public Carte(int val) {
+        this.__valeur = val;
+        this.__faceCachee = true;
     }
 
-    public int getValeur(){
-        return this.valeur;
+    // Getter de la classe Carte
+    public int getValeur() {
+        return this.__valeur;
     }
 
-    public void setValeur(int val){
-        this.valeur = val;
+    // Setter de la classe Carte
+    public void setValeur(int val) {
+        this.__valeur = val;
+    }
+
+    // Méthode pour retourner une carte, quand elle est révélée dans la grille ou
+    // envoyée dans la défausse
+    public void retoruner() {
+        this.__faceCachee = !this.__faceCachee;
     }
 }
